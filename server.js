5ed3191af400013c3854787b38d51a8c483b12da
madhur2004 <madhurchaturvedi04@gmail.com>
2025-04-02 14:12:05 +0530

    added student endpoint

--- server.js
+++ server.js
@@ -7,4 +7,29 @@ app.get("/", (req, res) => {
   res.status(200).json({ name: "agile-node-app" })
 });
 
+
+app.get("/student", (req, res) => {
+  res.status(200).json([{
+    name: "Madhur",
+    Age: 21,
+    College: "IIMT College of engineering "
+  },
+  {
+    name: "Anmol ",
+    Age: 21,
+    College: "IIMT College of engineering "
+    },
+    {
+      name: "Chetan",
+      Age: 23,
+      College: "IIMT College of engineering "
+    },
+    {
+      name: "Deepak",
+      Age: 22,
+      College: "IIMT College of engineering "
+    }
+  ])
+})
+
 app.listen(3000);
